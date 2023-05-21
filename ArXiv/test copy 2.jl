fd9c14b6604bd84dd67584b15da6c0750fd30a02
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-05-21 11:04:11 +0200

    Update test copy 2.jl

--- ArXiv/test copy 2.jl
+++ ArXiv/test copy 2.jl
@@ -1,57 +1,34 @@
-using Distributions
+function f(X, Q, u, t)
+    # Unpack state variables
+    x, y, psi, v = X
+    q = Q[1]
 
-# Define the parameters
-d = 2   # Dimension of the continuous component of the state
-NI = 2  # Number of possible discrete states
-U = [-1, 0, 1]  # Set of controls
-Tf = 10  # Final time
-dt = 0.01  # Time step size
+    # Define constants
+    c = 1.5
+    rho = 1.226
+    m = 300
+    Iz = 1000
+    L = 6
+    B = 2
+    g = 9.81
 
-# Define the continuous dynamics function
-function f(x, q, u)
-    if q == 1
-        return [x[1] + u[1]*dt, x[2] + u[2]*dt]
-    else
-        return [x[1] + u[2]*dt, x[2] + u[1]*dt]
-    end
-end
+    # Compute forces
+    T = u[1]
+    phi = u[2]
+    beta = psi - phi
+    Fa = 0.5 * rho * v^2 * c * L * B
+    Fr = -0.5 * rho * v^2 * L * B * sin(beta)
+    Fh = T
+    Fx = Fa + Fr - Fh * sin(beta)
+    Fy = Fh * cos(beta) - m * g
+    Mz = Fh * L * cos(beta) - Fr * L * sin(beta)
 
-# Define the switching times
-t1 = 3
-t2 = 6
-t3 = 9
+    # Compute state derivatives
+    dxdt = v * cos(psi)
+    dydt = v * sin(psi)
+    dpsidt = v * L / Iz * sin(beta)
+    dvdt = 1 / m * Fy
 
-# Define the piecewise constant function Q
-function Q(t)
-    if t < t1
-        return 1
-    elseif t < t2
-        return 2
-    elseif t < t3
-        return 1
-    else
-        return 2
-    end
-end
-
-# Define the initial conditions
-x0 = [0.0, 0.0]
-q0 = 1
-
-# Define the standard Brownian motion process
-W = zeros(d, Int(Tf/dt))
-dW = sqrt(dt)*randn(d, Int(Tf/dt))
-for i = 2:Int(Tf/dt)
-    W[:,i] = W[:,i-1] + dW[:,i-1]
-end
-
-# Simulate the system
-x = zeros(d, Int(Tf/dt))
-q = zeros(Int(Tf/dt))
-x[:,1] = x0
-q[1] = q0
-for i = 2:Int(Tf/dt)
-    u = rand(U)
-    x[:,i] = f(x[:,i-1], q[i-1], u) + dW[:,i-1]
-    q[i] = Q(i*dt)
+    dXdt = [dxdt; dydt; dpsidt; dvdt]
+    return dXdt
 end
